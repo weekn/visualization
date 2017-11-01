fad8d29617c58cbb9fd626fc85329168ba72478c
weekn <weekn@qq.com>
2017-11-01 09:22:05 +0800

    PDb

--- app/plugins/js/plugins.js
+++ app/plugins/js/plugins.js
@@ -1,33 +1,5 @@
-function loadContent(href) {
-  $.get(href, function(data) {
-    $("#project").html(data);
-  })
-};
-$(document).ready(function(){
-	loadContent("pro_traveler/main.html");
 
 
-});
 
-
-$(document).ready(function() {
-  var win_height = window.innerHeight;
-  var top_height = $("#top").height();
-  var main_content = $("#main_content").height();
-  var hv = (win_height-top_height) + 'px';
-
-  console.log(win_height, top_height, hv);
-  $('.main_content').css('height', hv)
-
-  $(window).resize(function() {
-    var win_height = window.innerHeight;
-    var top_height = $("#top").height();
-    var hv = (win_height-top_height) + 'px';
-    $('#main_content').css('height', hv)
-
-
-  });
-
-})
 
 
